8674914394eef03da1097f68a9e39735a3ebfc58
Iryna Kuptsova <irinakuptsova1708@gmail.com>
2026-08-12 13:52:29 +0200

    Format + improve console logs

--- dots.ts
+++ dots.ts
@@ -1,30 +1,27 @@
-// @ts-check
-
-/** 
- * @param {string} str
- * @returns {string[]}
-*/
 function pointsBinaryFunction(str: string) {
-     if (str.length <= 1) return [str];
-    
-    const combinations = 2 ** (str.length - 1);
-    const result = [];
-    
-    for (let i = 0; i < combinations; i++) {
-        const mask = i.toString(2).padStart(str.length - 1, '0');
-        let current = '';
-        
-        for (let j = 0; j < str.length; j++) {
-            current += str[j];
-            if (j < str.length - 1 && mask[j] === '1') {
-                current += '.';
-            }
-        }
-        result.push(current);
+  if (str.length <= 1) return [str];
+
+  const combinations = 2 ** (str.length - 1);
+  const result = [];
+
+  for (let i = 0; i < combinations; i++) {
+    const mask = i.toString(2).padStart(str.length - 1, "0");
+    let current = "";
+
+    for (let j = 0; j < str.length; j++) {
+      current += str[j];
+      if (j < str.length - 1 && mask[j] === "1") {
+        current += ".";
+      }
     }
-    
-    return result;
+    result.push(current);
+  }
+
+  return result;
 }
 
-console.log(pointsBinaryFunction('abc'));
-console.log(pointsBinaryFunction('abcd'));
+console.log(
+  pointsBinaryFunction("abcd")
+    .map((result, index) => `${index + 1}. ${result}`)
+    .join("\n"),
+);
